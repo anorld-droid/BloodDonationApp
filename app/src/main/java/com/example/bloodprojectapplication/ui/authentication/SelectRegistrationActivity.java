6210d618f3a31e409e11a7ec1e860fcf0d0d4b65
anorld-droid <mulindipatrice00@gmail.com>
2022-11-21 09:28:58 +0300

    Add admin reg.

--- app/src/main/java/com/example/bloodprojectapplication/ui/authentication/SelectRegistrationActivity.java
+++ app/src/main/java/com/example/bloodprojectapplication/ui/authentication/SelectRegistrationActivity.java
@@ -1,18 +1,18 @@
 package com.example.bloodprojectapplication.ui.authentication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.bloodprojectapplication.R;
 
 public class SelectRegistrationActivity extends AppCompatActivity {
 
-    private Button donorButton, recipientButton;
+    private Button donorButton, recipientButton, adminButton;
     private TextView backButton;
 
     @Override
@@ -23,6 +23,7 @@ public class SelectRegistrationActivity extends AppCompatActivity {
         donorButton = findViewById(R.id.donorButton);
         recipientButton = findViewById(R.id.recipientButton);
         backButton = findViewById(R.id.backButton);
+        adminButton = findViewById(R.id.adminButton);
 
         donorButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +47,13 @@ public class SelectRegistrationActivity extends AppCompatActivity {
                 Intent intent = new Intent(SelectRegistrationActivity.this, LoginActivity.class);
                 startActivity(intent);
 
+            }
+        });
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectRegistrationActivity.this, AdminRegistrationActivity.class);
+                startActivity(intent);
             }
         });
     }
